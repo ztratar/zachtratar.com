3c8d8a63d3c12561db465b47a8985f1a2f0e239e
zach <ztratar@gmail.com>
2013-03-20 22:45:14 -0700

    New site almost done

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,86 +1,47 @@
 $(function() {
-	'use strict'
-	var nav = $('#nav'),
-		windowHeight = $(window).height(),
-		navHeight = nav.height(),
-		placeNavTop,
-		mainSections = $('#blog, #about, #work'),
-		currentPageChangeTimeout = null,
-		scrollChangeClassCurrent;
 
-	// Place the about section of the page
-	placeNavTop = function() {
-		var resizeAmnt;
-		windowHeight = $(window).height();
-		resizeAmnt = Math.max(15, (windowHeight-navHeight)/2);
-		nav.css('top', resizeAmnt);
-		if (resizeAmnt === 15) {
-			nav.css('position', 'absolute');
-		} else {
-			nav.css('position', 'fixed');
-		}
-	};
-	
-	$(window).on('resize.nav', placeNavTop);
-	placeNavTop();
+	function moveItemLeft(index,item) {
+		$(item).animate({
+			left: $(item).css('left') - 10
+		}, 1000);
+	}
 
-	mainSections.addClass('forceAnimation').addClass('back');
+	function setUpCarousel() {
+		$('ul.img-carousel').each(function(item, elem) {
+			var newElem = $(elem).clone();
+			newElem.appendTo($(elem).parent());
+			(function() {
+				var containedElem = elem,
+					totalWidth = $(elem).width(),
+					animateFunction = function() {
+						$(containedElem).animate({
+							'margin-left': -totalWidth
+						}, 50000, 'linear', function() {
+							$(containedElem).css('margin-left', 0);
+							animateFunction();						
+						});
+					};
+					animateFunction();
+			})();
+		});
+	}
 
-	$("a.overwrite-url").on('click', function(e) {
+	setTimeout(function() {
+		setUpCarousel();	
+	}, 3000);
 
-		if (currentPageChangeTimeout) {
-			clearTimeout(currentPageChangeTimeout);
-			scrollChangeClassCurrent();	
-		}
-
-		var target = $(e.currentTarget),
-			urlAttr = target.attr('href'),
-			goToPage = urlAttr.replace('/', ''),
-			newPageElem = $('#' + goToPage),
-			currentlyOpen = $('section.current');
-
-		scrollChangeClassCurrent = function() {
-			currentlyOpen	
-				.scrollTop(0)
-				.addClass('forceAnimation')
-				.removeClass('forward')
-				.addClass('back');	
-			currentPageChangeTimeout = null;
-		};
-
-		// Already on ths page
-		if (currentlyOpen && currentlyOpen[0] === newPageElem[0]) {
-			return false;
-		}
-
-		$("a.overwrite-url").removeClass('active');
-		target.addClass('active');
-
-		currentlyOpen.removeClass('current');
-		currentlyOpen.addClass('forward');
-		currentPageChangeTimeout = setTimeout(scrollChangeClassCurrent, 500);
-
-		newPageElem
-			.removeClass('forceAnimation')
-			.removeClass('back')
-			.addClass('current');
-		nav.addClass('left');
-
+	$('.see-all-blog-posts').click(function() {
+		$('.blog-posts-minimal').hide();	
+		$('.blog-posts-full').show();	
 		return false;
 	});
 
-	if (document.URL.match(/\?post=/) !== null) {
-		var postUrl = document.URL.match(/\?post=(.+\.html)/);
-		if (postUrl.length > 1) {
-			$("body").css({ opacity: 0 });
-			postUrl = postUrl[1].replace('%2F','/');
-			$("a.red").click();
-			setTimeout(function() {
-				$("#blog").scrollTop($("li[data-url='"+postUrl+"']").position().top - 30);
-				$("body").animate({ opacity: 1 }, 400);
-			}, 1000);
-		}
-	}
+	$('.see-more-about').click(function() {
+		$(this).hide();
+		$('.more-about').show();	
+		return false;
+	});
+
 
 });
 
