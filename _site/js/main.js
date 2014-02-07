857358c11712e4c89ba132c2c8ac8d5270d8d2c1
Zach Tratar <ztratar@gmail.com>
2014-02-07 01:07:52 -0800

    Looks like the new version is pretty much DONE

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,10 +2,13 @@ $(function() {
 	// For blog post pages
 	var $addThis = $('.addthis_toolbox'),
 		$window = $(window),
-		windowHeight = $window.height();
+		windowHeight = $window.height(),
+		windowWidth = $window.width(),
+		widgetState = windowWidth > 940 ? 'large' : 'small';
 
 	$window.on('resize', _.throttle(function() {
 		windowHeight = $window.height();
+		windowWidth = $window.width();
 	}, 16));
 
 	if ($addThis) {
@@ -20,5 +23,29 @@ $(function() {
 			}	
 		}, 16));
 	}
+
+	$window.on('resize.socialWidgets', _.throttle(function() {
+		var widgetWidth = 450,
+			origWidgetState = widgetState,
+			commentsContainer = $('.fb-comments-container'),
+			pageUrl = commentsContainer ? commentsContainer.attr('data-page-url') : undefined;
+
+		if (windowWidth <= 960) {
+			widgetWidth = 300;
+			widgetState = 'small';
+		} else {
+			widgetState = 450;
+			widgetState = 'large';	
+		}
+
+		if (widgetState !== origWidgetState) {
+			$('.fb-follow-container').html('<div class="fb-follow" data-href="http://www.facebook.com/ztratar" data-colorscheme="dark" data-layout="standard" data-show-faces="true" data-width="' + widgetWidth + '"></div>');
+		}
+		if (pageUrl) {
+			$('.fb-comments-container').html('<div class="fb-comments" data-href="' + pageUrl + '" data-width="' + (widgetState === 'small' ? commentsContainer.width() : '700') + '" data-num-posts="10"></div>');
+		}
+
+		window.FB.XFBML.parse();
+	}, 200));
 });
 
